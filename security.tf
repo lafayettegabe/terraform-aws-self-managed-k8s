b0f0d45294743e650610e5d2d1c6a9fc205e8d4e
lafayettegabe <gabriel-soares@uergs.edu.br>
2025-01-14 06:59:07 -0300

    feat(security): add kube-router ports

--- security.tf
+++ security.tf
@@ -1,12 +1,44 @@
 resource "aws_security_group" "k8s_master_sg" {
   vpc_id = aws_vpc.main.id
 
-  # # SSH (for debugging)
+  # SSH (for debugging)
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # DNS
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # BGP (for kube-router)
+  ingress {
+    from_port   = 179
+    to_port     = 179
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # kube-router health checks
+  ingress {
+    from_port   = 20244
+    to_port     = 20244
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # IPIP (for pod networking)
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "4"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   # API Server
@@ -46,6 +78,37 @@ resource "aws_security_group" "k8s_worker_sg" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  # DNS
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # BGP (for kube-router)
+  ingress {
+    from_port   = 179
+    to_port     = 179
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # kube-router health checks
+  ingress {
+    from_port   = 20244
+    to_port     = 20244
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+
+  # IPIP (for pod networking)
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "4"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   # Kubelet
